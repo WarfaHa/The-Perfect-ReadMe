1ce12745cffcb5ce6c187b90d2cdb98e7b427c2c
Warfa <warfahassan35@yahoo.com>
2023-01-02 23:46:46 -0500

    generated markdown version of readme with sections

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,32 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if ((license = "Apache")) {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if ((license = "BSD 2-Clause")) {
+    return `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
+  } else if ((license = "BSD 3-Clause")) {
+    return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+  } else if ((license = "Boost")) {
+    return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
+  } else if ((license = "GNU LGPL")) {
+    return `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`;
+  } else if ((license = "GNU AGPL")) {
+    return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+  } else if ((license = "GNU GPL v2")) {
+    return `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+  } else if ((license = "GNU GPL v3")) {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if ((license = "MIT")) {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n\n`;
+  } else if ((license = "Mozilla")) {
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+  } else if ((license = "Unlicense")) {
+    return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+  } else if (!license) {
+    return ``;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,12 +34,45 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `## Licenses
+     To view the ${license} license. Click the license button above.`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.licenses)}
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Tests](#test)
+  * [Licenses](#licenses)
+  * [Contact](#contact)
+  * [Credits](#credits)
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.install}
+  ## Usage
+  ${data.usage}
+  ## Contribution
+  ${data.contribution}
+  ## Tests
+  ${data.test}
+  ${renderLicenseSection(data.licenses)}
+  ## Contact
+ If you have any additional questions, here's how to reach me.
+  GitHub: https://github.com/${data.user}  
+  Email: ${data.email}
+  ## Credits
+This README file was created by ${data.name}
 `;
 }
 
